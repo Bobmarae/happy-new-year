ffc4839718b175249231b7b97e8fddcc99f70c9f
Bobmarae <garfielddee49@gmail.com>
2026-01-01 00:20:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const overlay = document.getElementById("start");
+const overlay = document.getElementById("overlay");
 const audio = document.getElementById("bgm");
+const flowers = document.getElementById("flowers");
+const fireworks = document.getElementById("fireworks");
 
 let started = false;
 
@@ -8,8 +10,25 @@ overlay.addEventListener("click", () => {
   started = true;
 
   overlay.style.display = "none";
+  audio.play();
 
-  audio.play().catch(err => {
-    console.log("Audio blocked:", err);
-  });
+  // ดอกไม้ลอย
+  setInterval(() => {
+    const f = document.createElement("div");
+    f.className = "flower";
+    f.textContent = "🌸";
+    f.style.left = Math.random() * 100 + "vw";
+    flowers.appendChild(f);
+    setTimeout(() => f.remove(), 6000);
+  }, 400);
+
+  // พลุ
+  setInterval(() => {
+    const fw = document.createElement("div");
+    fw.className = "firework";
+    fw.style.left = Math.random() * 100 + "vw";
+    fw.style.top = Math.random() * 50 + "vh";
+    fireworks.appendChild(fw);
+    setTimeout(() => fw.remove(), 1200);
+  }, 800);
 });
